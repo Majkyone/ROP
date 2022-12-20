6805dc94628a652dd13314babae5f056ac348511
Majkyone <marian.sula2@gmail.com>
2022-12-20 15:19:46 +0100

    pridal som footer

--- products_site/products_site.php
+++ products_site/products_site.php
@@ -40,6 +40,7 @@
     <link rel="stylesheet" href="/ROp/products_site/style/products.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <?php include 'C:\xampp\htdocs\ROP\links.html'?>
 </head>
 <body>
     <nav class="navbar">
@@ -182,6 +183,7 @@
             </div>
         </div>
     </div>
+    <?php include 'C:\xampp\htdocs\ROP\footer.html'?>
 </body>
 </html>
 <script src="/ROP/main_page/js-file/script.js"></script>
